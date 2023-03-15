9f0d5bc6e21414df31894151db0dbac105de1316
unknown <querubij@email.sc.edu>
2023-03-15 17:31:26 -0400

    JSON implementation WIP

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,5 +1,63 @@
 package src;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.UUID;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;;
 
-public class DataLoader {
-    
+public class DataLoader extends DataConstants {
+    public static ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<User>();
+        try {
+            FileReader reader = new FileReader(COURSE_FILE_NAME);
+			JSONParser parser = new JSONParser();	
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+
+            for(int i=0; i < peopleJSON.size(); i++) {
+                JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+                String email = (String)personJSON.get(EMAIL);
+                String password = (String)personJSON.get(PASSWORD);
+				String userName = (String)personJSON.get(USERNAME);
+				String firstName = (String)personJSON.get(FIRST_NAME);
+				String lastName = (String)personJSON.get(LAST_NAME);
+                UUID id = UUID.fromString((String)personJSON.get(USER_ID));
+
+                users.add(new User(email, password, userName, firstName, lastName));
+            }
+
+            return users;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    public static ArrayList<Course> getCourses() {
+        ArrayList<Course> courses = new ArrayList<Course>();
+        try {
+            FileReader reader = new FileReader(USER_FILE_NAME);
+			JSONParser parser = new JSONParser();	
+			JSONArray courseJSON = (JSONArray)new JSONParser().parse(reader);
+
+            for(int i=0; i < courseJSON.size(); i++) {
+                JSONObject coursesJSON = (JSONObject)courseJSON.get(i);
+                String teacher = (String)coursesJSON.get(TEACHER);
+                int difficulty = (int)coursesJSON.get(DIFFICULTY);
+                ArrayList<Module> modules = (Module)coursesJSON.get(MODULES);
+                ArrayList<Comment> comments = (Comment)coursesJSON.get(COMMENTS);
+                Quiz endOfCourseQuiz = (Quiz)coursesJSON.get(END_OF_COURSE_QUIZ);
+                String courseID = (String)coursesJSON.get(COURSE_ID);
+                String userID = (String)coursesJSON.get(USER_ID);
+            }
+
+            return courses;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
--- src/DataWriter.java
+++ src/DataWriter.java
@@ -1,5 +1,77 @@
 package src;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-public class DataWriter {
-    
+public class DataWriter extends DataConstants {
+    public static void saveUsers() {
+		User users = User.getInstance();
+		ArrayList<User> userList = users.getUsers();
+		JSONArray jsonUsers = new JSONArray();
+		
+		//creating all the json objects
+		for(int i=0; i< userList.size(); i++) {
+			jsonUsers.add(getUserJSON(userList.get(i)));
+		}
+		
+		//Write JSON file
+        try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+ 
+            file.write(jsonUsers.toJSONString());
+            file.flush();
+ 
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+    public static JSONObject getUserJSON(User user) {
+		JSONObject userDetails = new JSONObject();
+		userDetails.put(USER_ID, user.getId().toString());
+		userDetails.put(USERNAME, user.getUserName());
+		userDetails.put(FIRST_NAME, user.getFirstName());
+		userDetails.put(LAST_NAME, user.getLastName());
+		userDetails.put(PASSWORD, user.getPassword().toString());
+        
+        return userDetails;
+	}
+
+    public static void saveCourses() {
+		Course courses = Course.getInstance();
+		ArrayList<User> courseList = courses.getCourses();
+		JSONArray jsonCourses = new JSONArray();
+		
+		//creating all the json objects
+		for(int i=0; i< CourseList.size(); i++) {
+			jsonCourses.add(getCourseJSON(courseList.get(i)));
+		}
+		
+		//Write JSON file
+        try (FileWriter file = new FileWriter(COURSE_FILE_NAME)) {
+ 
+            file.write(jsonCourses.toJSONString());
+            file.flush();
+ 
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+    public static JSONObject getUserJSON(User user) {
+		JSONObject courseDetails = new JSONObject();
+		courseDetails.put(TEACHER, user.getTeacher().toString());
+		courseDetails.put(DIFFICULTY, user.getDifficulty());
+		courseDetails.put(MODULES, user.getModule());
+		courseDetails.put(COMMENTS, user.getComments());
+		courseDetails.put(END_OF_COURSE_QUIZ, user.getEndOfCOurseQuiz().toString());
+        courseDetails.put(QUIZ_RESULT, user.getQuizResult());
+        courseDetails.put(COURSE_ID, user.getCourseID());
+        courseDetails.put(USER_ID, user.getUser());
+        
+        return courseDetails;
+	}
 }
