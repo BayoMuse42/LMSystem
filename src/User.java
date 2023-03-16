1454cf6c23d918349641c68cecd9b76079f92ae9
PluralOfGoose <111785913+PluralOfGoose@users.noreply.github.com>
2023-03-16 13:16:08 -0400

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package src;
 
 public abstract class User {
@@ -20,26 +19,3 @@ public abstract class User {
 
     
 }
-=======
-package src;
-
-public abstract class User {
-
-    private String email;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    public UUID userID;
-
-    public User(String email, String username, String password){};
-
-    public User(String email, String username, String password, UUID userID){};
-
-    public ArrayList<Course> getCourses(String keyword){};
-
-    public ArrayList<Course> getCourses(){};
-
-    
-}
->>>>>>> 5081bc48bc523f3cb034691f7e7c4d99ee4ca46c
