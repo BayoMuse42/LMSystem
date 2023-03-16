3e23841188127563802272e5f882e4a217a4f55c
PluralOfGoose <111785913+PluralOfGoose@users.noreply.github.com>
2023-03-16 13:18:50 -0400

    Update Module.java

--- src/Module.java
+++ src/Module.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package src;
 
 public class Module {
@@ -44,50 +44,3 @@ public class Module {
     public double getQuizResult
     
 }
-=======
-package src;
-
-public class Module {
-
-    private String name;
-    private String description;
-    private Quiz quiz;
-    private double quizResult;
-    private ArrayList<Section> sections;
-
-    public Module(String name, String description){
-
-    }
-
-    public void createSection(String name, String content){
-
-    }
-
-    public String getName(){
-
-    }
-
-    public String getContent(){
-
-    }
-
-    public void setName(String name){
-
-    }
-
-    public void setContent(String message){
-
-    }
-
-    public boolean isComplete(){
-
-    }
-
-    public void createQuiz(){
-
-    }
-
-    public double getQuizResult
-    
-}
->>>>>>> 5081bc48bc523f3cb034691f7e7c4d99ee4ca46c
