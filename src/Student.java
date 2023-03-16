5a90c557a224009e5399112f55b0e2e1051a0d63
PluralOfGoose <111785913+PluralOfGoose@users.noreply.github.com>
2023-03-16 13:18:07 -0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package src;
 
 import java.util.ArrayList;
@@ -25,31 +24,3 @@ public class Student {
     }
     
 }
-=======
-package src;
-
-import java.util.ArrayList;
-
-public class Student {
-
-    private int skill;
-    private ArrayList<Grade> grades;
-
-    public Student(String email, String username, String password){
-
-    }
-
-    public Student(String email, String username, String password, UUID userID){
-
-    }
-
-    public ArrayList<Course> getCourses(String keyword){
-
-    }
-
-    public ArrayList<Course> getCourses(){
-        
-    }
-    
-}
->>>>>>> 5081bc48bc523f3cb034691f7e7c4d99ee4ca46c
