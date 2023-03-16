494674bee3cb860886f2ba2dd530d2fb34bebd1f
PluralOfGoose <111785913+PluralOfGoose@users.noreply.github.com>
2023-03-16 13:18:28 -0400

    Update Quiz.java

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package src;
 
 public class Quiz {
@@ -14,20 +13,3 @@ public class Quiz {
     }
     
 }
-=======
-package src;
-
-public class Quiz {
-
-    public ArrayList<Question> questions;
-
-    public Quiz(Question question){
-
-    }
-
-    public void addQuestion(String ask, ArrayList<String> answers, int correctAnswer){
-        
-    }
-    
-}
->>>>>>> 5081bc48bc523f3cb034691f7e7c4d99ee4ca46c
