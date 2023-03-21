a57ac3db7af38e368ba37b6542e7bd7ce835e22b
PluralOfGoose <willrb@email.sc.edu>
2023-03-21 13:47:44 -0400

    WB 03/21

--- src/Quiz.java
+++ src/Quiz.java
@@ -2,21 +2,25 @@ package src;
 import java.util.ArrayList;
 
 public abstract class Quiz {
-    public ArrayList<Question> question;
-    protected String ask;
-    protected int correctAnswer;
-    protected ArrayList<String> answer;
+
+    public ArrayList<Question> questions;
 
     public Quiz(Question question) {
-
+        questions = new ArrayList<Question>();
+        questions.add(question);
     }
+
     /*
      * this class add questions in the parm of ask
      * answer, and the correct answer. 
-     *      */
-    public void addQuestion(String ask,ArrayList<String> answer, int correctAnswer) {
-        this.ask = ask;
-        
-    } 
+     *      
+    public void addQuestion(String ask, ArrayList<String> potentialAnswer, String answer) {
+        Question question = new Question(ask, answer, potentialAnswer);
+        this.questions.add(question);
+    } */
+
+    public Quiz getQuiz(){
+
+    }
 
 }
