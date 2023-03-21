40efd353a3c4846696317aef856a7f791457aceb
csriley <csriley@email.sc.edu>
2023-03-21 13:56:50 -0400

    update on quiz class

--- src/Quiz.java
+++ src/Quiz.java
@@ -2,24 +2,14 @@ package src;
 import java.util.ArrayList;
 
 public abstract class Quiz {
-    public ArrayList<Question> question;
+    
     protected String ask;
     protected int correctAnswer;
     protected ArrayList<String> answer;
     protected Object ArrayList;
 
     public Quiz(Question question) {
-
+         question.ask(question);
     }
-    /*
-     * this class add questions in the parm of ask
-     * answer, and the correct answer. 
-     *      */
-    public void addQuestion(String ask, ArrayList<String> answer, int correctAnswer) {
-        this.ask = ask;
-        this.ArrayList<String> = answer;
-        this.correctAnswer = correctAnswer;
-        
+    
     } 
-
-}
