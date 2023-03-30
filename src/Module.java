4921bb51a27936951b6ff2620ccb7141cfbb0c44
BayoMuse42 <bayomuse42@gmail.com>
2023-03-30 19:52:08 -0400

    debug

--- src/Module.java
+++ src/Module.java
@@ -18,6 +18,18 @@ public class Module {
      * @param description
      * Constructor takes name and description
      */
+    public Module(String name, String description, Quiz quiz){
+        this.name = name;
+        this.description = description;
+        this.quiz = quiz;
+    }
+
+    /**
+     * 
+     * @param name
+     * @param description
+     * Constructor takes name and description
+     */
     public Module(String name, String description){
         this.name = name;
         this.description = description;
@@ -102,6 +114,10 @@ public class Module {
      */
     public Quiz getQuiz(){
         return this.quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
     }
 
     /**
